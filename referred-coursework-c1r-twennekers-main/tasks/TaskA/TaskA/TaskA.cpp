80d83645ca4acf8d8829758e8bdb6fe175cf18a1
FeintWitch <feintwitch@gmail.com>
2024-07-24 19:48:12 +0100

    double check

--- referred-coursework-c1r-twennekers-main/tasks/TaskA/TaskA/TaskA.cpp
+++ referred-coursework-c1r-twennekers-main/tasks/TaskA/TaskA/TaskA.cpp
@@ -10,7 +10,7 @@ using namespace std;
 //See bottom of main
 int findArg(int argc, char* argv[], string pattern);
 
-///new comment
+///new comment now checking
 /*
  *
  * The user can launch application as follows:
