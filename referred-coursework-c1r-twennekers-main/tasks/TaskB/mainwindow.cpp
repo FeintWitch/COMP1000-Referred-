a5361a4cf4c1624f12f9f67022acbf5df8bb61ca
FeintWitch <feintwitch@gmail.com>
2024-08-13 18:33:10 +0100

    more updated

--- referred-coursework-c1r-twennekers-main/tasks/TaskB/mainwindow.cpp
+++ referred-coursework-c1r-twennekers-main/tasks/TaskB/mainwindow.cpp
@@ -11,3 +11,32 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
 }
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_openFileButton_clicked()
+{
+    QString fileName = "computing.txt";
+    if (openAndReadFile(fileName)){
+        ui->resultsTextEdit->append("file loaded successfully");
+    } else {
+        ui->resultsTextEdit->append("error");
+    }
+}
+
+void MainWindow::on_saveResultsButton_clicked()
+{
+    saveResultsToFile("results.csv");
+}
+
+///This will allow to open, read and store text
+///
+///********
+bool MainWindow::openAndReadFile(const QString &FileName)
+{
+
+}
+
