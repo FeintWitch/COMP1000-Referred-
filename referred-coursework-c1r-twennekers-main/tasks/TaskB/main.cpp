2a09050f62bc04a344388616b8561b8cf9a5c25d
FeintWitch <feintwitch@gmail.com>
2024-08-15 01:31:56 +0100

    small changes

--- referred-coursework-c1r-twennekers-main/tasks/TaskB/main.cpp
+++ referred-coursework-c1r-twennekers-main/tasks/TaskB/main.cpp
@@ -1,11 +1,28 @@
-#include "mainwindow.h"
+////// although the main code for gui and additonal stuff is in mainwindow
+///// the plan is to use this call mainwindow code for command line just in case
+///// I am wrong
 
 #include <QApplication>
+#include <QCommandLineParser>
+#include "mainwindow.h"
 
-int main(int argc, char *argv[])
-{
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    return a.exec();
+int main(int argc, char *argv[]){
+    QApplication app(argc, argv);
+        MainWindow w;
+        w.show();
+
+        QCommandLineParser parser;
+        parser.setApplicationDescription("qt app with CLI commands");
+        parser.addHelpOption();
+
+        //comand line
+
+        QCommandLineOption runAddRecordOption("runAddRecord");
+        QCommandLineOption runUpdateRecordOption("runUpdateRecord");
+        QCommandLineOption runQueryDBOption("runQueryDB");
+        QCommandLineOption showAllOption("showAll");
+
+        // options
+        parser
+
 }
